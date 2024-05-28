db37d66fa45466315445984b63de2072df2f1393
Hepatica <75219332+danilt2000@users.noreply.github.com>
2024-05-29 00:03:36 +0200

    Add files via upload

---
+++ GenericsDemo/GenericsDemo/Generic.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericsDemo
+{
+    public class Generic<T> where T : IDoable
+    {
+        private T doable;
+
+        public Generic(T doable)
+        {
+            this.doable = doable;
+        }
+
+        public void UseDoable() {
+            doable.Do();
+        }
+    }
+}
